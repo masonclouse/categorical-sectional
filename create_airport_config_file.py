8f68945337ef5dc2cc4381847f20c58ec4af31ed
Mason Clouse <63482302+masonclouse@users.noreply.github.com>
2020-04-20 23:44:05 -0400

    Turns a list of airports into the airport config file.

---
+++ create_airport_config_file.py
@@ -0,0 +1,34 @@
+# Creates the airport config file from a text list of airport identifiers.
+
+# INPUT file name:
+file = open('data/FILENAME.txt', 'r')
+
+list = []
+for line in file:
+    list.append(str(line))
+
+list = [w.replace('\n', '') for w in list]
+count = 0
+
+# OUTPUT file name:
+output = open("data/airport_config_file.json", 'w')
+
+output.write("{")
+output.write("\n")
+output.write("  \"ws2811\": [")
+output.write("\n")
+
+for line in list:
+    output.write("    { \"")
+    output.write(line)
+    output.write("\": { \"neopixel\": ")
+    output.write(str(count))
+    output.write(" } }")
+    if count < len(list) - 1:
+        output.write(",")
+    output.write("\n")
+    count += 1
+
+output.write("  ]")
+output.write("\n")
+output.write("}")
